1d7102a91b2b7fb5c7b43ce04870a4c81005f484
Christian Capeans <christiancp100@gmail.com>
2022-07-23 12:59:33 +0200

    DO Spaces

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -3,11 +3,12 @@ module.exports = ({ env }) => ({
     config: {
       provider: "strapi-provider-upload-do",
       providerOptions: {
-        key: process.env.DO_SPACE_ACCESS_KEY,
-        secret: process.env.DO_SPACE_SECRET_KEY,
-        endpoint: process.env.DO_SPACE_ENDPOINT,
-        space: process.env.DO_SPACE_BUCKET,
-        directory: "media", // optional
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_BUCKET"),
+        directory: env("DO_SPACE_DIRECTORY"),
+        cdn: env("DO_SPACE_CDN"),
       },
     },
   },

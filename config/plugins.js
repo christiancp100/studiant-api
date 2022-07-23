cd4fdded8233ac78f786f7e9181ed9941a6f946c
Christian Capeans <christiancp100@gmail.com>
2022-07-23 13:18:06 +0200

    Move DO spaces configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,15 @@
 module.exports = ({ env }) => ({
   upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
-    },
-    actionOptions: {
-      upload: {},
-      delete: {},
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_BUCKET"),
+        directory: env("DO_SPACE_DIRECTORY"),
+        cdn: env("DO_SPACE_CDN"),
+      },
     },
   },
 });
